64f5351ca69d9b9e32b49c4f9a76643b7caf0e2b
Tore Darell <toredarell@gmail.com>
2009-03-12 16:11:03 +0100

    Add i18n load path to the beginning to allow values to be overridden

--- lib/simple_authentication.rb
+++ lib/simple_authentication.rb
@@ -3,4 +3,4 @@ require "simple_authentication/controller_methods"
 require "simple_authentication/model_methods"
 
 ApplicationController.send(:include, SimpleAuthentication::ControllerMethods::Application)
-I18n.load_path << File.join(File.dirname(__FILE__), '..', 'config', 'locales', 'simple_authentication.yml')
+I18n.load_path.unshift(File.join(File.dirname(__FILE__), '..', 'config', 'locales', 'simple_authentication.yml'))

c400c059c0f6718d62234c1580a7baae5e4e9317
Tore Darell <toredarell@gmail.com>
2009-03-14 01:21:25 +0100

    new_login_with_authenticator should have lower priority than new_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.new_login_with_authenticator 'login/:authenticator', :controller => 'logins', :action => 'new'
   map.resource :login
+  map.new_login_with_authenticator 'login/:authenticator', :controller => 'logins', :action => 'new'
 end

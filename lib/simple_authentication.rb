da6a2910f73e532945c866361664c36ff2700ae9
Tore Darell <toredarell@gmail.com>
2009-03-15 21:10:15 +0100

    Don't load ApplicationController

--- lib/simple_authentication.rb
+++ lib/simple_authentication.rb
@@ -2,5 +2,5 @@ require "simple_authentication/authenticator"
 require "simple_authentication/controller_methods"
 require "simple_authentication/model_methods"
 
-ApplicationController.send(:include, SimpleAuthentication::ControllerMethods::Application)
+ActionController::Base.send(:include, SimpleAuthentication::ControllerMethods::Application)
 I18n.load_path.unshift(File.join(File.dirname(__FILE__), '..', 'config', 'locales', 'simple_authentication.yml'))
